602add462f4e267e8287e1413ff5ae98a91a4cf6
peaga <pauloherbert@alu.ufc.br>
2024-05-15 16:11:00 -0300

    setup project

---
+++ src/main/java/org/peagadev/loadingps2024/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package org.peagadev.loadingps2024.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.anyRequest().permitAll())
+                .build();
+    }
+}
