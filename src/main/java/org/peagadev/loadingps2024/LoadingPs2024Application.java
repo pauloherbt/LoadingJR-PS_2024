af9751d39901b69949c486bf413d47a8b1e3d231
peaga <pauloherbert@alu.ufc.br>
2024-05-15 16:10:13 -0300

    setup project

---
+++ src/main/java/org/peagadev/loadingps2024/LoadingPs2024Application.java
@@ -0,0 +1,13 @@
+package org.peagadev.loadingps2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadingPs2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadingPs2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/peagadev/loadingps2024/LoadingPs2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.peagadev.loadingps2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadingPs2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
