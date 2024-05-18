22c92c37be933ebb40d6f9ac0e5cd8b972d7b4ba
peaga <pauloherbert@alu.ufc.br>
2024-05-18 10:55:55 -0300

    add: authorization

--- src/main/java/org/peagadev/loadingps2024/domain/entities/User.java
+++ src/main/java/org/peagadev/loadingps2024/domain/entities/User.java
@@ -1,5 +1,6 @@
 package org.peagadev.loadingps2024.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,10 +8,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -19,17 +24,48 @@ import java.util.Set;
 @NoArgsConstructor
 @Data
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
     @CreationTimestamp
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user")
     private Set<Post> posts = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/org/peagadev/loadingps2024/domain/repository/UserRepository.java
+++ src/main/java/org/peagadev/loadingps2024/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.peagadev.loadingps2024.domain.repository;
 import org.peagadev.loadingps2024.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
